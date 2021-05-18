7ee6e8dd1368ce12617154fcbae333179e74c0bc
Ratchapol Sukonthamarn <sushicud55@gmail.com>
2021-05-18 19:31:33 +0700

    Add description for FoundationPile.java

--- src/layout/FoundationPile.java
+++ src/layout/FoundationPile.java
@@ -8,13 +8,13 @@ import card.CardStacks;
 import card.Value;
 
 public class FoundationPile {
-	private final Map<Foundation, CardStacks> foundMap = new HashMap<>();
+	private final Map<Foundation, CardStacks> foundMap = new HashMap<>(); // A map of foundations.
 
-	public FoundationPile() {
+	public FoundationPile() { // Initializing foundation piles when called.
 		initialize();
 	}
 
-	public int getTotalSize() {
+	public int getTotalSize() { // Returns the total number of cards in foundation piles.
 		int sum = 0;
 		for (CardStacks stack : foundMap.values()) {
 			sum += stack.size();
@@ -22,18 +22,18 @@ public class FoundationPile {
 		return sum;
 	}
 
-	public void initialize() {
+	public void initialize() { // Initializing four foundation piles.
 		for (Foundation index : Foundation.values()) {
 			foundMap.put(index, new CardStacks());
 		}
 	}
 
-	public boolean isEmpty(Foundation location) {
+	public boolean isEmpty(Foundation location) { // Checks if that foundation pile is empty.
 		assert location != null;
 		return foundMap.get(location).isEmpty();
 	}
 
-	public boolean canMoveTo(Card card, Foundation location) {
+	public boolean canMoveTo(Card card, Foundation location) { // Checks if we can move a card to that foundation pile.
 		assert card != null && location != null;
 		if (isEmpty(location)) {
 			return card.getVALUE() == Value.ACE;
@@ -43,17 +43,17 @@ public class FoundationPile {
 		}
 	}
 
-	public Card peek(Foundation location) {
+	public Card peek(Foundation location) { // Returns a topmost card of that foundation pile.
 		assert location != null && !foundMap.get(location).isEmpty();
 		return foundMap.get(location).peek();
 	}
 
-	public void push(Card card, Foundation location) {
+	public void push(Card card, Foundation location) { // Places a card on the foundation pile.
 		assert card != null && location != null;
 		foundMap.get(location).push(card);
 	}
 
-	public Card pop(Foundation location) {
+	public Card pop(Foundation location) { // Removes and returns a topmost card of that foundation pile.
 		assert location != null && !isEmpty(location);
 		return foundMap.get(location).pop();
 	}
