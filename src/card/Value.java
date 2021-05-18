9e2c50b2dd12342b718071d1fea25d1836090c4e
Ratchapol Sukonthamarn <sushicud55@gmail.com>
2021-05-18 17:56:24 +0700

    Add description for Value.java

--- src/card/Value.java
+++ src/card/Value.java
@@ -1,5 +1,5 @@
 package card;
 
-public enum Value {
+public enum Value { // Value of each card. Range from 1 (Ace) to 13 (King).
 	ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
 }
