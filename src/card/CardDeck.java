68451cb8d7319658945f04420be5940888b8910a
Ratchapol Sukonthamarn <sushicud55@gmail.com>
2021-05-18 19:22:04 +0700

    Add description for CardDeck.java

--- src/card/CardDeck.java
+++ src/card/CardDeck.java
@@ -5,13 +5,13 @@ import java.util.Collections;
 import java.util.List;
 
 public class CardDeck {
-	private CardStacks deck;
+	private CardStacks deck; // A card deck.
 
-	public CardDeck() {
+	public CardDeck() { // Shuffles the deck when initialize it.
 		shuffle();
 	}
 
-	public void shuffle() {
+	public void shuffle() { // Reinitializes the deck.
 		List<Card> cards = new ArrayList<>();
 		for (Suit suit : Suit.values()) {
 			for (Value value : Value.values()) {
@@ -22,17 +22,17 @@ public class CardDeck {
 		deck = new CardStacks(cards);
 	}
 
-	public void push(Card card) {
+	public void push(Card card) { // Pushes a card on top of the deck.
 		assert card != null;
 		deck.push(card);
 	}
 
-	public Card draw() {
+	public Card draw() { // Draws and removes a card from the deck.
 		assert !isEmpty();
 		return deck.pop();
 	}
 
-	public boolean isEmpty() {
+	public boolean isEmpty() { // Checks if the deck is empty.
 		return deck.isEmpty();
 	}
 }
