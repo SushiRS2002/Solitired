aec6967f1851e539f402bf17977c0da3caed99d0
Ratchapol Sukonthamarn <sushicud55@gmail.com>
2021-05-20 16:40:21 +0700

    Add description for DragHandler.java

--- src/gui/DragHandler.java
+++ src/gui/DragHandler.java
@@ -9,7 +9,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
 
 public class DragHandler implements EventHandler<MouseEvent> { // Stores a string represents the dragged card.
-	private static final ClipboardContent CLIPBOARD_CONTENT = new ClipboardContent();
+	private static final ClipboardContent CLIPBOARDCONTENT = new ClipboardContent();
 	private Card card; // A card
 	private ImageView image; // A card's image
 
@@ -22,10 +22,10 @@ public class DragHandler implements EventHandler<MouseEvent> { // Stores a strin
 	}
 
 	@Override
-	public void handle(MouseEvent me) {
+	public void handle(MouseEvent me) { // Handle any drag event.
 		Dragboard db = image.startDragAndDrop(TransferMode.ANY);
-		CLIPBOARD_CONTENT.putString(card.getIDString());
-		db.setContent(CLIPBOARD_CONTENT);
+		CLIPBOARDCONTENT.putString(card.getIDString());
+		db.setContent(CLIPBOARDCONTENT);
 		me.consume();
 	}
 }
